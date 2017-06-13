274721c63fb7c34da46222d681ec90c7c99c14fc
Mike Tsou <mr.tsou@gmail.com>
2017-06-12 18:20:21 -0700

    Add delete functionality for questions, fix create functionality for all answer types, enable answer show capability on user show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :new, :show, :create, :edit] do
-    resources :questions, only: [:show, :new, :create, :destroy] do
+    resources :questions, only: [:index, :show, :new, :create, :destroy] do
       resources :text_answers, only: [:show, :new, :create]
       resources :boolean_answers, only: [:show, :new, :create]
       resources :integer_answers, only: [:show, :new, :create]
