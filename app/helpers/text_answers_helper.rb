68336438ef3b9aea0d730516e35cd9408bf5107b
iand11 <ian.driscoll.o@gmail.com>
2017-06-15 14:47:06 -0700

    Add style to register link

--- app/helpers/text_answers_helper.rb
+++ app/helpers/text_answers_helper.rb
@@ -2,23 +2,23 @@ module TextAnswersHelper
 
   include TimezoneHelper
 
-def day_check_text(q)
-  n = 0
-  day_hashes = []
-  until n == 7
-    check = []
-    q.text_answers.each do |text|
-      if text.created_at.strftime('%Y%m%d').to_i == time_converter.strftime('%Y%m%d').to_i - n
-        check << text
-      end
-  end
-    if check.empty?
-      day_hashes << {(Chronic.parse("#{n} day ago").strftime('%a')) => false}
-    else
-      day_hashes << {(Chronic.parse("#{n} day ago").strftime('%a')) => check[0].response}
+  def day_check_text(q)
+    n = 0
+    day_hashes = []
+    until n == 7
+      check = []
+      q.text_answers.each do |text|
+        if text.created_at.strftime('%Y%m%d').to_i == time_converter.strftime('%Y%m%d').to_i - n
+          check << text
+        end
     end
-    n += 1
+      if check.empty?
+        day_hashes << {(Chronic.parse("#{n} day ago").strftime('%a')) => false}
+      else
+        day_hashes << {(Chronic.parse("#{n} day ago").strftime('%a')) => check[0].response}
+      end
+      n += 1
+    end
+    day_hashes
   end
-  day_hashes
-end
 end
